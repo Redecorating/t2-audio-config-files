780afa01e6acce827836aaeff2c8fd5ecaf5960c
Redecorating <69827514+Redecorating@users.noreply.github.com>
2021-02-15 09:15:00 +1100

    apple-t2.conf now put in two places
    pulseaudio and pipewire have different directories for card profiles,
    now it'll put the profile in both places.

--- install.sh
+++ install.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 srcDir=normal
 Model=$(cat /sys/devices/virtual/dmi/id/product_name)
-profileDir=/usr/share/pulseaudio/alsa-mixer/profile-sets
 
-echo This script isn\'t well tested, ^C at the read prompt if you don\'t want to run it
-read foo
-
+umask 644
 
 if [ $Model == "MacBookPro16,1" ] || [ $Model == "MacBookPro16,4" ]
 	then echo This computer is a $Model, which had 6 speakers and requires different audio configuration files, which will be installed.
 	srcDir=16-1
-
 fi
 echo Moving files...
-cp ./$srcDir/AppleT2.conf /usr/share/alsa/cards/AppleT2.conf
-cp ./$srcDir/91-apple-t2-audio.rules /usr/lib/udev/rules.d/91-apple-t2-audio.rules
-# At least on arch, the alsa-card-profiles package was split off from pulseaudio, and as a result, the directory for apple-t2.conf has changed
-if test -d /usr/share/alsa-card-profile/mixer/profile-sets 
-then profileDir=/usr/share/alsa-card-profile/mixer/profile-sets
-fi
-cp ./$srcDir/apple-t2.conf $profileDir/apple-t2.conf
+cp -v ./$srcDir/AppleT2.conf /usr/share/alsa/cards/AppleT2.conf
+cp -v ./$srcDir/91-apple-t2-audio.rules /usr/lib/udev/rules.d/91-apple-t2-audio.rules
+# If you are using pipewire or some older versions of pulseaudio, the directory for apple-t2.conf is different.
+# If we put it in both, it should work for everyone.
+mkdir -p /usr/share/alsa-card-profile/mixer/profile-sets
+mkdir -p /usr/share/pulseaudio/alsa-mixer/profile-sets
+
+cp -v ./$srcDir/apple-t2.conf /usr/share/alsa-card-profile/mixer/profile-sets/apple-t2.conf
+cp -v ./$srcDir/apple-t2.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/apple-t2.conf
 
 if [ srcDir == 16-1 ]
-	then echo You are on a $Model, and if you are using pulseaudio, you\'ll need to make some changes to configuration files. Please refer to this gist: https://gist.github.com/kevineinarsson/8e5e92664f97508277fefef1b8015fba
+	then echo You are on a $Model, and if you are using pulseaudio, you\'ll need to make some changes to your pulseaudio configuration files. Please refer to this gist: https://gist.github.com/kevineinarsson/8e5e92664f97508277fefef1b8015fba
 fi
